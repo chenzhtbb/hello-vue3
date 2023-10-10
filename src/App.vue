554ce4fba1c7836adde30661a361bdc2c457f305
chenzhtbb <chenzhtbb@163.com>
2023-10-10 22:15:47 +0800

    完善目录

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,53 @@
 <template>
-  <n-config-provider :locale="zhCN" :hljs="hljs">
-    <n-global-style />
-    <div class="main box">
-      <n-grid x-gap="12" :cols="2">
-        <n-grid-item>
-          <n-cascader v-model:value="selectDemo" check-strategy="child" :options="options" placeholder="请选择要查看的演示项目" />
-        </n-grid-item>
-        <n-grid-item>
-          <!-- <n-button type="info" @click="showCodePanel">代码面板</n-button> -->
-        </n-grid-item>
-      </n-grid>
-    </div>
-    <n-divider title-placement="center">{{ selectDemoText }}</n-divider>
-    <div class="main footer">
-      <component :is="autoImportComponent" v-model="selectDemo" />
-    </div>
-  </n-config-provider>
+  <div>
+    <n-config-provider :locale="zhCN" :hljs="hljs">
+      <n-global-style />
+      <div class="main box">
+        <n-grid x-gap="12" :cols="2">
+          <n-grid-item>
+            <n-cascader v-model:value="selectDemo" check-strategy="child" :options="options" placeholder="请选择要查看的演示项目" />
+          </n-grid-item>
+        </n-grid>
+      </div>
+      <n-divider title-placement="center">{{ selectDemoText }}</n-divider>
+      <div class="main footer">
+        <component :is="autoImportComponent" v-model="selectDemo" />
+      </div>
+    </n-config-provider>
+  </div>
 </template>
 
 <script setup>
-import { computed, defineAsyncComponent, ref } from 'vue';
-import {
-  NConfigProvider,
-  zhCN,
-  NGlobalStyle,
-  NCascader,
-} from 'naive-ui'
+import { zhCN } from 'naive-ui'
 import hljs from 'highlight.js/lib/core'
 import javascript from 'highlight.js/lib/languages/javascript'
 import html from 'highlight.js/lib/languages/xml'
+import { onMounted, watch, ref, computed, defineAsyncComponent } from 'vue';
 hljs.registerLanguage('js', javascript)
 hljs.registerLanguage('html', html)
 
+const __ROUTER_STORE__ = '__ROUTER_STORE__'
+
 const selectDemo = ref(null)
+
 const selectDemoText = computed(() => {
   return selectDemo.value ? `演示项目 ${(selectDemo.value).replace(/-/g, '.')}` : null
 })
+
 const autoImportComponent = computed(() => {
   return !selectDemo.value ? null : defineAsyncComponent(() => import(`@/components/${selectDemo.value}.vue`))
 })
 
-const showCodePanel = () => {
+watch(selectDemo, (newVal) => {
+  localStorage.setItem(__ROUTER_STORE__, newVal)
+})
 
-}
+onMounted(() => {
+  const select = localStorage.getItem(__ROUTER_STORE__)
+  if (select) {
+    selectDemo.value = select
+  }
+})
 
 const options = [
   {
@@ -201,7 +206,7 @@ const options = [
           },
           {
             value: '3-5-3',
-            label: '3.5.3 v-if 对比 v-show 指令'
+            label: '3.5.3 v-if 对比 v-show'
           },
         ]
       },
@@ -249,27 +254,357 @@ const options = [
   }, {
     value: '4',
     label: '第4章',
-    children: []
+    children: [
+      {
+        value: '4-1',
+        label: '4.1 绑定 HTML class',
+        children: [
+          {
+            value: '4-1-1',
+            label: '4.1.1 对象语法'
+          },
+          {
+            value: '4-1-2',
+            label: '4.1.2 数组语法'
+          }
+        ]
+      }, {
+        value: '4-2',
+        label: '4.2 绑定内联样式',
+        children: [
+          {
+            value: '4-2-1',
+            label: '4.2.1 对象语法'
+          },
+          {
+            value: '4-2-2',
+            label: '4.2.2 数组语法'
+          }
+        ]
+      },
+    ]
   }, {
     value: '5',
     label: '第5章',
-    children: []
+    children: [
+      {
+        value: '5-1',
+        label: '5.1 过滤器',
+        children: [
+          {
+            value: '5-1-1',
+            label: '5.1.1 字母过滤器'
+          },
+          {
+            value: '5-1-2',
+            label: '5.1.2 json 过滤器'
+          },
+          {
+            value: '5-1-3',
+            label: '5.1.3 限制过滤器'
+          },
+          {
+            value: '5-1-4',
+            label: '5.1.4 currency 过滤器'
+          },
+          {
+            value: '5-1-5',
+            label: '5.1.5 debounce 过滤器'
+          }
+        ]
+      },
+    ]
   }, {
     value: '7',
     label: '第7章',
-    children: []
+    children: [
+      {
+        value: '7-1',
+        label: '7.1 组件的注册',
+        children: [
+          {
+            value: '7-1-1',
+            label: '7.1.1 全局注册'
+          }, {
+            value: '7-1-1',
+            label: '7.1.1 局部注册'
+          },
+        ]
+      }, {
+        value: '7-2',
+        label: '7.2 组件的数据传递',
+        children: [
+          {
+            value: '7-2-1',
+            label: '7.2.1 props 参数'
+          }, {
+            value: '7-2-2',
+            label: '7.2.2 组件通信'
+          }, {
+            value: '7-2-3',
+            label: '7.2.3 v-model 参数'
+          }, {
+            value: '7-2-4',
+            label: '7.2.4 Vue 3.0 中的 v-model 修饰符'
+          },
+        ]
+      }, {
+        value: '7-3',
+        label: '7.3 插槽内容分发',
+        children: [
+          {
+            value: '7-3-1',
+            label: '7.3.1 插槽的基本用法'
+          }, {
+            value: '7-3-2',
+            label: '7.3.2 插槽的作用域'
+          }, {
+            value: '7-3-3',
+            label: '7.3.3 插槽的后备内容'
+          }, {
+            value: '7-3-4',
+            label: '7.3.4 具名插槽'
+          }, {
+            value: '7-3-5',
+            label: '7.3.5 作用域插槽'
+          },
+        ]
+      }, {
+        value: '7-4',
+        label: '7.4 动态组件',
+        children: [
+          {
+            value: '7-4-1',
+            label: '7.4.1 动态组件的基础用法'
+          }, {
+            value: '7-4-2',
+            label: '7.4.2 <keep-alive>'
+          },
+        ]
+      }, {
+        value: '7-5',
+        label: '7.5 组件案例：完善标签页组件',
+      }
+    ]
   }, {
     value: '8',
     label: '第8章',
-    children: []
+    children: [
+      {
+        value: '8-1',
+        label: '8.1 vue-router 的基本用法',
+        children: [
+          {
+            value: '8-1-1',
+            label: '8.1.1 vue-router 的安装'
+          }, {
+            value: '8-1-2',
+            label: '8.1.2 vue-router 的基本使用'
+          }, {
+            value: '8-1-3',
+            label: '8.1.3 跳转'
+          },
+        ]
+      }, {
+        value: '8-2',
+        label: '8.2 动态路由匹配',
+        children: [
+          {
+            value: '8-2-1',
+            label: '8.2.1 带参数的动态路由匹配'
+          }, {
+            value: '8-2-2',
+            label: '8.2.2 响应参数变化'
+          }, {
+            value: '8-2-3',
+            label: '8.2.3 参数全匹配'
+          },
+        ]
+      }, {
+        value: '8-3',
+        label: '8.3 路由匹配的语法',
+        children: [
+          {
+            value: '8-3-1',
+            label: '8.3.1 自定义正则表达式'
+          }, {
+            value: '8-3-2',
+            label: '8.3.2 可选参数'
+          }, {
+            value: '8-3-3',
+            label: '8.3.3 可重复参数'
+          },
+        ]
+      }, {
+        value: '8-4',
+        label: '8.4 嵌套路由',
+      }, {
+        value: '8-5',
+        label: '8.5 命名路由',
+      }, {
+        value: '8-6',
+        label: '8.6 重定向和别名',
+        children: [
+          {
+            value: '8-6-1',
+            label: '8.6.1 重定向'
+          }, {
+            value: '8-6-2',
+            label: '8.6.2 别名'
+          },
+        ]
+      }, {
+        value: '8-7',
+        label: '8.7 向路由组件传递参数',
+        children: [
+          {
+            value: '8-7-1',
+            label: '8.7.1 向路由组件传递参数的基本语法'
+          }, {
+            value: '8-7-2',
+            label: '8.7.2 传递参数的模式'
+          },
+        ]
+      },
+    ]
   }, {
     value: '9',
     label: '第9章',
-    children: []
+    children: [
+      {
+        value: '9-1',
+        label: '9.1 Pinia 简介',
+        children: [
+          {
+            value: '9-1-1',
+            label: '9.1.1 状态管理模式'
+          }, {
+            value: '9-1-2',
+            label: '9.1.2 安装 Pinia'
+          }, {
+            value: '9-1-3',
+            label: '9.1.3 Pinia 的基本使用'
+          },
+        ]
+      }, {
+        value: '9-2',
+        label: '9.2 Pinia 中的状态',
+        children: [
+          {
+            value: '9-2-1',
+            label: '9.2.1 单一状态树'
+          }, {
+            value: '9-2-2',
+            label: '9.2.2 将 Pinia 状态加入 Vue 组件'
+          }, {
+            value: '9-2-3',
+            label: '9.2.3 mapState 的使用'
+          }, {
+            value: '9-2-4',
+            label: '9.2.4 组件的本地状态'
+          },
+        ]
+      }, {
+        value: '9-3',
+        label: '9.3 Pinia 中的 getter',
+        children: [
+          {
+            value: '9-3-1',
+            label: '9.3.1 仓库的 getter'
+          }, {
+            value: '9-3-2',
+            label: '9.3.2 属性式访问'
+          }, {
+            value: '9-3-3',
+            label: '9.3.3 方法式访问'
+          }, {
+            value: '9-3-4',
+            label: '9.3.4 mapGetter 的使用'
+          },
+        ]
+      }, {
+        value: '9-4',
+        label: '9.4 Pinia 中的 mutation',
+        children: [
+          {
+            value: '9-4-1',
+            label: '9.4.1 mutation 的有效负载'
+          }, {
+            value: '9-4-2',
+            label: '9.4.2 通过对象提交'
+          }, {
+            value: '9-4-3',
+            label: '9.4.3 mutation 的同步'
+          },
+        ]
+      }, {
+        value: '9-5',
+        label: '9.5 Pinia 中的 action',
+        children: [
+          {
+            value: '9-5-1',
+            label: '9.5.1 action 的基本使用'
+          }, {
+            value: '9-5-2',
+            label: '9.5.2 调度 action'
+          }, {
+            value: '9-5-3',
+            label: '9.5.3 组成 action'
+          },
+        ]
+      }, {
+        value: '9-6',
+        label: '9.6 Pinia 中的模块',
+      },
+    ]
   }, {
     value: '10',
     label: '第10章',
-    children: []
+    children: [
+      {
+        value: '10-1',
+        label: '10.1 项目目录介绍',
+        children: [
+          {
+            value: '10-1-1',
+            label: '10.1.1 dist 文件夹'
+          }, {
+            value: '10-1-2',
+            label: '10.1.2 node_modules 文件夹'
+          }, {
+            value: '10-1-3',
+            label: '10.1.3 src 文件夹'
+          },
+        ]
+      }, {
+        value: '10-2',
+        label: '10.2 前端页面开发',
+        children: [
+          {
+            value: '10-2-1',
+            label: '10.2.1 Vue 文件'
+          }, {
+            value: '10-2-2',
+            label: '10.2.2 导入 import'
+          },
+        ]
+      }, {
+        value: '10-3',
+        label: '10.3 项目目录介绍',
+        children: [
+          {
+            value: '10-3-1',
+            label: '10.3.1 项目打包'
+          }, {
+            value: '10-3-2',
+            label: '10.3.2 项目部署'
+          }, {
+            value: '10-3-3',
+            label: '10.3.3 通过 GitHub Action 自动部署'
+          },
+        ]
+      },
+    ]
   },
 ]
 </script>
