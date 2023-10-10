62f0f20a47286975a00165f1816d402329f6643a
chenzhtbb <chenzhtbb@163.com>
2023-10-10 22:15:29 +0800

    更新项目基本信息

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@
  * @Author: chenzhtbb chenzhtbb@163.com
  * @Date: 2023-10-07 09:58:28
  * @LastEditors: chenzhtbb chenzhtbb@163.com
- * @LastEditTime: 2023-10-10 16:31:06
+ * @LastEditTime: 2023-10-10 20:48:31
  * @FilePath: /hello-vue3/src/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const app = createApp(App)
-app.mount('#app')
+createApp(App).mount('#app')
 
