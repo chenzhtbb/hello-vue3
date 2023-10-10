bc8fab413d23bb2c59b8ebfc90f487954d9ddd5b
chenzhtbb <chenzhtbb@163.com>
2023-10-10 17:52:15 +0800

    添加 naive-ui 自动引入配置

--- src/main.js
+++ src/main.js
@@ -1,45 +1,14 @@
+/*
+ * @Author: chenzhtbb chenzhtbb@163.com
+ * @Date: 2023-10-07 09:58:28
+ * @LastEditors: chenzhtbb chenzhtbb@163.com
+ * @LastEditTime: 2023-10-10 16:31:06
+ * @FilePath: /hello-vue3/src/main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
-import {
-  // create naive ui
-  create,
-  // component
-  NButton,
-  NGrid,
-  NGridItem,
-  NCode,
-  NDivider,
-  NText,
-  NBlockquote,
-  NA,
-  NTabs,
-  NTabPane,
-  NImage,
-  NSwitch,
-  NTable,
-  NSpace,
-} from 'naive-ui'
-
-const naive = create({
-  components: [
-    NButton,
-    NGrid,
-    NGridItem,
-    NCode,
-    NDivider,
-    NText,
-    NBlockquote,
-    NA,
-    NTabs,
-    NTabPane,
-    NImage,
-    NSwitch,
-    NTable,
-    NSpace,
-  ]
-})
 
 const app = createApp(App)
-app.use(naive)
 app.mount('#app')
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,43 @@
-import { fileURLToPath, URL } from 'node:url'
+/*
+ * @Author: chenzhtbb chenzhtbb@163.com
+ * @Date: 2023-10-07 09:58:28
+ * @LastEditors: chenzhtbb chenzhtbb@163.com
+ * @LastEditTime: 2023-10-10 16:29:43
+ * @FilePath: /hello-vue3/vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: [
+        "vue",
+        {
+          "naive-ui": [
+            "useDialog",
+            "useMessage",
+            "useNotification",
+            "useLoadingBar",
+          ],
+        },
+      ],
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
