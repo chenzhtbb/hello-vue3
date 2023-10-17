e47c599e831ff574b4c69136dac34acc77802bcc
chenzhtbb <chenzhtbb@163.com>
2023-10-17 17:11:32 +0800

    更新v-on

--- src/components/3-7-1.vue
+++ src/components/3-7-1.vue
@@ -2,11 +2,11 @@
  * @Author: chenzhtbb chenzhtbb@163.com
  * @Date: 2023-10-07 09:58:28
  * @LastEditors: chenzhtbb chenzhtbb@163.com
- * @LastEditTime: 2023-10-11 22:47:29
+ * @LastEditTime: 2023-10-16 21:49:17
  * @FilePath: /hello-vue3/src/components/3-7-1.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
-<template>
+<template @keydown="action = '监听按下任意键'">
   <h1>3.7.1 v-on 指令</h1>
   <h2>监听事件</h2>
   <p>使用 <n-text type="primary">v-on</n-text> 指令 (简写为 <n-text code>@</n-text>) 来监听 DOM 事件，并在事件触发时执行对应的
@@ -50,11 +50,41 @@
       </tr>
     </tbody>
   </n-table>
+  <h2>监听实例</h2>
+  <p style="font-size: 48px;">监听动作：<n-gradient-text type="info">
+      {{ action }}
+    </n-gradient-text></p>
+  <div class="box-div" @click="action = '监听单击'">监听单击</div>
+  <div class="box-div" @dblclick="action = '监听双击'">监听双击</div>
+  <div class="box-div" @contextmenu="action = '监听右击'">监听右击</div>
+  <div class="box-div" @mouseover="action = '监听指针移入'">监听指针移入</div>
+  <div class="box-div" @mouseout="action = '监听指针移出'">监听指针移出</div>
+  <p>下面这两个事件单纯监听是无效的</p>
+  <div class="box-div" @keydown="action = '监听按下任意键'">监听按下任意键</div>
+  <div class="box-div" @keyup="action = '监听释放任意键'">监听释放任意键</div>
+  <p>要这样使用</p>
+  <div class="box-div">
+    <div><span>监听按下回车键: </span><input @keyup.enter="action = '监听按下回车键'" type="text"></div>
+    <div><span>监听按下Alt+任意键: </span><input @keyup.alt="action = '监听按下Alt+任意键'" type="text"></div>
+    <div><span>监听按下Shift+任意键: </span><input @keyup.shift="action = '监听按下Shift+任意键'" type="text"></div>
+    <div><span>监听按下Ctrl+任意键: </span><input @keyup.ctrl="action = '监听按下Ctrl+任意键'" type="text"></div>
+  </div>
 </template>
 
 <script setup>
-
+import { ref } from 'vue'
 defineOptions({
-    inheritAttrs: false
+  inheritAttrs: false
 })
+const action = ref('')
 </script>
+
+<style>
+.box-div {
+  padding: 8px;
+  margin: 8px 0;
+  background-color: chocolate;
+  font-size: 18px;
+  cursor: pointer;
+}
+</style>
--- src/components/3-7-2.vue
+++ src/components/3-7-2.vue
@@ -38,16 +38,118 @@
         <td>事件被触发一次后即解除监昕</td>
       </tr>
       <tr>
-        <td>.passlve</td>
+        <td>.passive</td>
         <td>滚动</td>
         <td>移动端，限制事件永不调用 <n-text code>preventDefault()</n-text> 方法</td>
       </tr>
     </tbody>
   </n-table>
+  <h2>事件修饰符实例：普通事件</h2>
+  <div class="div1" @click="onClick('你点击了底部div')">
+    <p>这是一个div标签，他有一个普通的点击事件</p>
+    <div class="div2" @click="onClick('你点击了重叠div')">
+      <p>这是一个重叠的div标签</p>
+    </div>
+  </div>
+  <br><br><br><br>
+  <h2>事件修饰符实例：stop 修饰</h2>
+  <div class="div1" @click="onClick('你点击了底部div')">
+    <p>这是一个div标签，他有一个普通的点击事件</p>
+    <div class="div2" @click.stop="onClick('你点击了重叠div，但是被stop修饰了')">
+      <p>这是一个重叠的div标签</p>
+    </div>
+  </div>
+  <br><br><br><br>
+  <h2>事件修饰符实例：prevent 修饰</h2>
+  <div class="div1">
+    <a href="https://www.gxmzu.edu.cn" target="_blank">这是一个超链接，它会跳转到广西民族大学官网</a>
+    <a href="https://www.gxmzu.edu.cn" target="_blank" @click.prevent="onClick('这个超链接被阻止了默认行为')">这是一个超链接，但是它没办法跳转</a>
+  </div>
+  <h2>事件修饰符实例：capture 修饰</h2>
+  <div class="div1" @click.capture="onClick('你点击了底部div')">
+    <p>这是一个div标签，他有一个普通的点击事件</p>
+    <div class="div2" @click="onClick('你点击了重叠div，由于底部div被capture修饰了，所以它第二个才弹出来')">
+      <p>这是一个重叠的div标签</p>
+    </div>
+  </div>
+  <br><br><br><br>
+  <h2>事件修饰符实例：self 修饰</h2>
+  <div class="div1" @click.self="onClick('你点击了底部div，但是被self修饰了')">
+    <p>这是一个div标签，他有一个普通的点击事件</p>
+    <div class="div2" @click="onClick('你点击了重叠div')">
+      <p>这是一个重叠的div标签</p>
+    </div>
+  </div>
+  <br><br><br><br>
+  <h2>事件修饰符实例：once 修饰</h2>
+  <div class="div1" @click.once="onClick('你点击了底部div，但是被once修饰了')">
+    <p>这是一个div标签，他有一个普通的点击事件</p>
+    <div class="div2" @click.once="onClick('你点击了重叠div，但是被once修饰了')">
+      <p>这是一个重叠的div标签</p>
+    </div>
+  </div>
+  <br><br><br><br>
+  <h2>事件修饰符实例：passive 修饰</h2>
+  <h3>下方的div标签内部可以滚动，并在滚动过程中出发scroll时间</h3>
+  <div @scroll="onScroll('正在滚动')" style="border: 4px red solid; height: 100px; overflow-y: scroll; width: 100px;">
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
+  </div>
+  <h3>下方的div标签内部可以滚动，但是被passive修饰了</h3>
+  <div @scroll.passive="onScroll('正在滚动passive，你会发现加不加passive没有任何变化')" style="border: 4px red solid; height: 100px; overflow-y: scroll; width: 100px;">
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
 defineOptions({
-    inheritAttrs: false
+  inheritAttrs: false
 })
+function onClick(msg = '你触发了一个点击事件') {
+  alert(msg)
+}
+function onScroll(msg) {
+  console.log(msg);
+}
 </script>
+
+<style scoped>
+a {
+  display: block;
+}
+
+.div1 {
+  position: relative;
+  background-color: #00CCFF;
+  padding: 16px;
+}
+
+.div2 {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: #66FF99;
+  padding: 16px;
+}
+</style>
